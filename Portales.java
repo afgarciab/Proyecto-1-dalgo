fae4db2fb40ade7913b51b6fdc1beee3e98394fc
andres <af.garciab@uniandes.edu.co>
2022-04-01 10:38:55 -0500

    ya estan las funciones base para que vallas trabajando

---
+++ Portales.java
@@ -0,0 +1,36 @@
+
+public class Portales {
+
+	/**
+	 * posicion x
+	 */
+	private int x;
+	/**
+	 * posicion y
+	 */
+	private int y;
+
+	public Portales(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
--- Proyecto1.java
+++ Proyecto1.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Hashtable;
+
 /**
  * 
  */
@@ -8,14 +11,84 @@
  */
 public class Proyecto1 {
 
+	private int[][] torre; 
+
+	private static Hashtable<Portales, Portales> contenedor;
+	
+	private static Hashtable<Integer, Integer> energia;
+	
+	private int sumaEnergias;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		contenedor=new Hashtable<Portales, Portales>();
+		energia = new Hashtable();
+		
+		//debo hacer la carga de datos.
+		//luego se pone la funcion inicializarTablaCostosEnergia con su parametro.
+		//luego se pone la funcion inicializarPortales con su parametro.
+		//luego se pone la funcion inicializarTorre.
+		//luego se pone la funcion calcularComplejidad.
 	}
-	
-	
+	/**
+	 * 
+	 * @param T = arreglo que contiene en [0] el numero de pisos,[1]num de cuartos y [2] numero Portales
+	 */
+	public void inicializarTorre(String[] T)
+	{
+		int pisos = Integer.parseInt(T[0]);//numero de pisos
+		int cuartos = Integer.parseInt(T[2]);//numero de cuartos
+		int portales = Integer.parseInt(T[0]);//numero de portales
+		int aproxInfinito = pisos*cuartos*portales*sumaEnergias;
+		for (int i =1;i<=pisos;i++)
+		{
+			for (int j=1;j<=cuartos;j++)
+			{
+				torre[i][j]=aproxInfinito;
+				if(i==1&&j==1)
+				{
+					torre[i][j]=0;
+				}
+
+			}
+		}
+
+	}/**
+	 * inicializa una tabla de hash con los portales
+	 * @param PT = matriz que contiene dentro un String que contiene la posicion inicial y final de cada portal.
+	 * */
+	public void inicializarPortales( String[] PT)
+	{
+		for (String x: PT)
+		{
+			String[] posicionesPortales = x.split(",");
+			int[] numeros = Arrays.stream(posicionesPortales).mapToInt(f->Integer.parseInt(f)).toArray();
+			Portales portalEntrada = new Portales(numeros[0],numeros[1]);
+			Portales portalSalida = new Portales(numeros[2],numeros[3]);
+			contenedor.put(portalEntrada, portalSalida);
+		} 
+	}
+	/**
+	 * inicializa una tabla de hash con las energias de cada piso.
+	 * @param costoEnergia
+	 */
+	public void inicializarTablaCostosEnergia(int[] costoEnergia)
+	{
+		for (int i=1; i<=costoEnergia.length;i++) {
+		energia.put(i, costoEnergia[i]);
+		sumaEnergias+=costoEnergia[i];
+		}
+	}
+
+	/**
+	 * 
+	 * @return la ruta mas corta
+	 */
+	public int calcularComplejidad(  )
+	{
+		return 0;
+	}
 
 }
